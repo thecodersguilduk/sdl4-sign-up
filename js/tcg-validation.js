448d3954d9445c53c884fd91d797fcda7e61a527
Steve Marks <stevenmarks75@gmail.com>
2021-07-06 15:40:00 +0100

    add extra validation check to dob field

--- js/tcg-validation.js
+++ js/tcg-validation.js
@@ -143,6 +143,9 @@ jQuery(document).on("click", ".validateFormStepNext, .submitApplication", functi
                 } else {
                     blnValidation = true;
                     switch(key){
+                        case 'date_of_birth':
+                            blnValidation = isValidDate(inputValue);
+                        break;
                         case 'postcode':
                             blnValidation = isValidPostcode(inputValue);
                         break;
@@ -247,6 +250,35 @@ function isValidateEmail(email) {
 function isValidateNationalInsurance(nationalInsurance) {
     const re = /^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$/;
     return re.test(String(nationalInsurance).toLowerCase());
+}
+
+function isValidDate(date){
+        var currVal = date;
+        if (currVal == '')
+            return false;
+        var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/; 
+        var dtArray = currVal.match(rxDatePattern);
+
+        if (dtArray == null)
+            return false;
+        
+        dtMonth = dtArray[1];
+        dtDay = dtArray[3];
+        dtYear = dtArray[5];
+
+        if (dtMonth < 1 || dtMonth > 12)
+            return false;
+        else if (dtDay < 1 || dtDay > 31)
+            return false;
+        else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31)
+            return false;
+        else if (dtMonth == 2) {
+            var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+            if (dtDay > 29 || (dtDay == 29 && !isleap))
+                return false;
+        }
+        return true;
+ 
 }
 
 jQuery( document ).ready(function() {
